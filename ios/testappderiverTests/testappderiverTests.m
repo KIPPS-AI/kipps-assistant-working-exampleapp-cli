7dd87cb28f36945685bfa172d9cc5e8ed8b5a1d6
narutoadi <mis.ad1206@gmail.com>
2024-11-12 17:10:53 +0530

    changes after renaming the app

--- ios/testappderiverTests/testappderiverTests.m
+++ ios/testappderiverTests/testappderiverTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KippsMonkTests : XCTestCase
+@interface testappderiverTests : XCTestCase
 
 @end
 
-@implementation KippsMonkTests
+@implementation testappderiverTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
